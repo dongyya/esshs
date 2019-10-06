88adbc9069170d873c9b6b98733bec13d50d9b8d
dongyya <dongyya@yonyou.com>
2019-10-06 19:03:54 +0800

    初始化

---
+++ src/com/entity/Identity.java
@@ -0,0 +1,30 @@
+package com.entity;
+
+import java.io.Serializable;
+
+/**
+ *	用户权限实体类
+ */
+public class Identity implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String identityName; //权限名
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getIdentityName() {
+		return identityName;
+	}
+	public void setIdentityName(String identityName) {
+		this.identityName = identityName;
+	}
+	
+}
